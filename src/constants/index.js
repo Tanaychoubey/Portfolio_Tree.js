51b945fd0505fc14793fe9e6c468489c8a5c9b98
Tanay Choubey <76908226+Tanaychoubey@users.noreply.github.com>
2023-07-02 23:57:30 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -189,7 +189,7 @@ const testimonials = [
 
 const projects = [
   {
-    name: "Car Rent",
+    name: "Pizza Delivery Web App",
     description:
       "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
     tags: [
